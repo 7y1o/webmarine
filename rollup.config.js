19f5b5076d53b068220fc70ed75a62a1f5a2d600
7y1o <tdesu.webdesign@gmail.com>
2021-06-05 02:03:41 +0300

    Updated configuration of a Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,49 @@
 import babel from "@rollup/plugin-babel";
 import typescript from "@rollup/plugin-typescript";
 import {wasm} from '@rollup/plugin-wasm';
-import glsl from "rollup-plugin-glsl";
 import filesize from "rollup-plugin-filesize";
 import {terser} from "rollup-plugin-terser";
+import sizes from 'rollup-plugin-sizes';
+import nodeResolve from "rollup-plugin-node-resolve";
+import progress from "rollup-plugin-progress";
 
 export default {
-    input: 'src/main.ts',
+    input: 'src/index.ts',
     output: {
-        file: 'build/index.js',
-        format: 'es'
+        file: 'build/webmarine.js',
+        format: 'esm'
     },
     plugins: [
+        typescript({
+            target: 'esnext',
+            module: 'esnext'
+        }),
         babel({
-            babelHelpers: 'bundled'
+            babelHelpers: 'inline'
         }),
-        typescript(),
         wasm(),
-        glsl({
-            sourceMap: false
+        nodeResolve({
+            browser: true
         }),
-        filesize(),
         terser({
-            ecma: 2020,
-            compress: true,
+            compress: {
+                arrows: true,
+                arguments: false,
+                booleans: true,
+                booleans_as_integers: true,
+                collapse_vars: true,
+                dead_code: true,
+                keep_classnames: true,
+                keep_fnames: true,
+                keep_fargs: true
+            },
             keep_classnames: true,
             keep_fnames: true,
-            safari10: true
-        })
+            safari10: false,
+            toplevel: true,
+        }),
+        progress(),
+        sizes(),
+        filesize()
     ]
 }
