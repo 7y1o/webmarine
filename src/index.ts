75bc4dbab0191473652e28f4f7786ab097188be4
7y1o <tdesu.webdesign@gmail.com>
2021-06-05 00:02:27 +0300

    Switched to Vite

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-// Render
-export {RenderConfig} from './render/RenderConfig';
-export {BuildRenderConfig} from './render/RenderConfig';
-export {InitializeRender} from './render/InitializeRender';
-
-// Utils
-export {ObjectOperations} from './utils/ObjectOperations';
