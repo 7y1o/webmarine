ea1a6f1aa1c444a8852b80b72523e97f14d68dd2
7y1o <tdesu.webdesign@gmail.com>
2021-06-10 01:25:25 +0300

    Removed example file for testing webpack

--- src/index.js
+++
@@ -1,5 +0,0 @@
-export class Test {
-    lol() {}
-}
-
-export * from './second';
--- src/second.js
+++
@@ -1,8 +0,0 @@
-export class Test2 {
-    ping() {}
-}
-
-/** Test 3 class */
-export class Test3 {
-    pong() {}
-}
