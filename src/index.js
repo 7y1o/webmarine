04f0b6b98c1c95585c3a0c507a6b87ef3eea6f59
7y1o <tdesu.webdesign@gmail.com>
2021-06-09 18:56:48 +0300

    Recreated with webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export class Test {
+    lol() {}
+}
+
+export * from './second';
---
+++ src/second.js
@@ -0,0 +1,8 @@
+export class Test2 {
+    ping() {}
+}
+
+/** Test 3 class */
+export class Test3 {
+    pong() {}
+}
