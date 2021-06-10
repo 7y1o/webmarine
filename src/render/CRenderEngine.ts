7237e9135d91f716398d48e777073cc42f5578a3
7y1o <tdesu.webdesign@gmail.com>
2021-06-10 12:11:44 +0300

    Embedding canvas in a page; Camera and scene change functions; Stage getters and cameras; Start render and tick

--- src/render/CRenderEngine.ts
+++ src/render/CRenderEngine.ts
@@ -1,9 +1,8 @@
-import {
-    Camera, PerspectiveCamera,
-    Scene,
-    WebGLRenderer
-} from "three";
+import {Camera, PerspectiveCamera, Scene, WebGLRenderer} from "three";
 import IRenderConfig from "./references/IRenderConfig";
+import IRenderEngineSwitch from "./references/IRenderEngineSwitch";
+import chalk from "chalk";
+import CLogger, {Logtype} from "../utils/CLogger";
 
 /**
  * @brief Class Render Engine
@@ -13,7 +12,7 @@ export default class CRenderEngine {
     private render: WebGLRenderer;
     private cCam: Camera;
     private cScene: Scene;
-    private tickSteps: ((number) => void)[];
+    private readonly tickSteps: ((number) => void)[];
 
     /** Construct CRenderEngine */
     public constructor(config?: IRenderConfig) {
@@ -38,5 +37,93 @@ export default class CRenderEngine {
 
         // Initialize tick steps array
         this.tickSteps = [];
+
+        // Add canvas to the page
+        document.body.innerHTML = '';
+        playCanvas.style.cssText = `
+            position: fixed:
+            z-index: 100;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100vh;
+            padding: 0;
+            margin: 0;
+        `;
+        document.body.append(playCanvas);
+    }
+
+    /** Switch current camera */
+    public switchCamera(newCam: Camera): void {
+        this.cCam = newCam;
+    }
+
+    /** Switch current scene */
+    public switchScene(newScene: Scene): void {
+        this.cScene = newScene;
+    }
+
+    /** Switch current camera or scene */
+    public switch(query: IRenderEngineSwitch): void {
+        // Switch camera if exists
+        if (query.camera) {
+            this.cCam = query.camera;
+        }
+
+        // Switch scene if exists
+        if (query.scene) {
+            this.cScene = query.scene;
+        }
+
+        // Show warning if query data doesn't contains scene or camera
+        if (!query.scene && !query.camera) {
+            CLogger.show(`used ${chalk.underline('switch')} with empty data in query`, Logtype.WARN);
+        }
+    }
+
+    /** Get access to the scene */
+    public get scene(): Scene {
+        return this.cScene;
+    }
+
+    /** Get access to the scene */
+    public get camera(): Camera {
+        return this.cCam;
+    }
+
+    /** Add step to the tick. Warning: don't push hard algorithms, it may decrease performance! */
+    public pushTickStep(fn: (number) => void): void {
+        this.tickSteps.push(fn);
+    }
+
+    /** Prepare render before start. Compiles WASM scripts, shaders and a little bit more actions.
+     * After all running render loop */
+    public prepareRenderAndStart(): void {
+        this.start();
+    }
+
+    /** Start render */
+    public start(): void {
+        let beginTime = Date.now();
+        this.loop(beginTime);
+    }
+
+    /** Run loop */
+    private loop(linkBeginTime: number): void {
+        let now = 0;
+        let dt = 0;
+
+        const tick = (): void => {
+            now = Date.now();
+            dt = now - linkBeginTime;
+            linkBeginTime = now;
+
+            for (const cb of this.tickSteps) {
+                cb(dt);
+            }
+
+            this.render.render(this.cScene, this.cCam);
+            requestAnimationFrame(() => tick());
+        }
     }
 }
