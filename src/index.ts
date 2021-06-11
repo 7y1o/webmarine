4d5fd438108af31781c4228899101742105b70c3
7y1o <tdesu.webdesign@gmail.com>
2021-06-11 23:56:46 +0300

    Added imports for CBaseEntity and IRenderEngineSwitch

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,8 @@
 
 // Game engine utils
 export {default as IRenderConfig} from './render/references/IRenderConfig';
+export {default as IRenderEngineSwitch} from './render/references/IRenderEngineSwitch';
 export {default as CRenderEngine} from './render/CRenderEngine';
+
+// Entity
+export {default as CBaseEntity} from './entity/CBaseEntity';
