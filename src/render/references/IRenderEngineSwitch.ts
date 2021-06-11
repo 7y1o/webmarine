333fa72056f9efc83d7d5286273785cf81340521
7y1o <tdesu.webdesign@gmail.com>
2021-06-11 23:57:00 +0300

    Export move to the bottom

--- src/render/references/IRenderEngineSwitch.ts
+++ src/render/references/IRenderEngineSwitch.ts
@@ -2,10 +2,12 @@ import type {Camera, Scene} from "three";
 import {ArrayCamera, OrthographicCamera, PerspectiveCamera, StereoCamera} from "three";
 
 /** Switch scene or camera query reference */
-export default interface IRenderEngineSwitch {
+interface IRenderEngineSwitch {
     /** Whether switch camera */
     camera?: PerspectiveCamera | ArrayCamera | StereoCamera | OrthographicCamera;
 
     /** Whether switch scene */
     scene?: Scene;
 }
+
+export default IRenderEngineSwitch;
