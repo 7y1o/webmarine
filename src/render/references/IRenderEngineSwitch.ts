254d27f1c84b5b796ba7a8bf50b161a1486b150f
7y1o <tdesu.webdesign@gmail.com>
2021-06-11 18:20:11 +0300

    Changed camera type to certain

--- src/render/references/IRenderEngineSwitch.ts
+++ src/render/references/IRenderEngineSwitch.ts
@@ -1,9 +1,10 @@
 import type {Camera, Scene} from "three";
+import {ArrayCamera, OrthographicCamera, PerspectiveCamera, StereoCamera} from "three";
 
 /** Switch scene or camera query reference */
 export default interface IRenderEngineSwitch {
     /** Whether switch camera */
-    camera?: Camera;
+    camera?: PerspectiveCamera | ArrayCamera | StereoCamera | OrthographicCamera;
 
     /** Whether switch scene */
     scene?: Scene;
