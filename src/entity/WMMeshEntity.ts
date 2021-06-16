d3c52eb1166ccabe6fa2153b350410338b7990d7
7y1o <tdesu.webdesign@gmail.com>
2021-06-16 22:10:12 +0300

    Added THREE.Group to entity types

--- src/entity/WMMeshEntity.ts
+++ src/entity/WMMeshEntity.ts
@@ -1,24 +1,24 @@
 /** Mesh entity builder */
 import {
     ConeGeometry,
-    CylinderGeometry,
+    CylinderGeometry, Group,
     Material,
     Mesh,
     MeshBasicMaterial,
     SphereGeometry,
-    TorusGeometry
+    TorusGeometry, Vector3
 } from "three";
 
 export class WMMeshEntity {
     /** Mesh instance */
-    private instance: Mesh;
+    private instance: Mesh | Group;
 
     /** Children entities */
     private children: WMMeshEntity[];
 
     /** Constructor */
-    public constructor(type: Mesh | 'cube' | 'sphere' | 'torus' | 'cylinder' | 'cone', material?: Material) {
-        if (type instanceof Mesh) {
+    public constructor(type: Mesh | Group | 'cube' | 'sphere' | 'torus' | 'cylinder' | 'cone', material?: Material) {
+        if (type instanceof Mesh || type instanceof Group) {
             this.instance = type;
         } else {
             const mat = material ?? new MeshBasicMaterial({
@@ -49,5 +49,40 @@ export class WMMeshEntity {
         }
     }
 
-    /**  */
+    /** Apply new material to the instance */
+    public set material(material: Material) {
+        if (!(this.instance instanceof Group)) {
+            this.instance.material = material;
+        } else {
+            console.warn('[WMC] ')
+        }
+    }
+
+    /** Position of the entity */
+    public get position() {
+        return {
+            /** Get position of the entity as Vector3 class */
+            get: () => this.instance.position,
+
+            /** Teleport entity to the new position */
+            set: (x: number, y: number, z: number) => {
+                this.instance.position.set(x, y, z);
+                for (const child of this.children) {
+                    child.position.set(
+                        child.position.get().x + x,
+                        child.position.get().y + y,
+                        child.position.get().z + z,
+                    );
+                }
+            },
+
+            /** Move entity */
+            add: (x: number, y: number, z: number) => {
+                this.instance.position.add(new Vector3(x, y, z));
+                for (const child of this.children) {
+                    child.position.add(x, y, z);
+                }
+            }
+        }
+    }
 }
