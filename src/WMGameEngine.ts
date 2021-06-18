c3bcee16d13460860904759f9dc0f6f45702b4dd
7y1o <tdesu.webdesign@gmail.com>
2021-06-18 22:23:35 +0300

    Now variables in game engine class in readonly; added getters

--- src/WMGameEngine.ts
+++ src/WMGameEngine.ts
@@ -5,13 +5,47 @@ import {WMAIEngine} from "./ai/WMAIEngine";
 
 /** Main entry point for game engine */
 export class WMGameEngine {
-    private renderEngine: WMRenderEngine;
-    private physicsEngine: WMPhysicsEngine | null;
-    private audioEngine: WMAudioEngine | null;
-    private aiEngine: WMAIEngine | null;
+    private readonly renderEngine: WMRenderEngine;
+    private readonly physicsEngine: WMPhysicsEngine;
+    private readonly audioEngine: WMAudioEngine;
+    private readonly aiEngine: WMAIEngine;
 
     /** Create game engine instance */
-    public constructor() {
-        this.renderEngine = new WMRenderEngine();
+    public constructor(config?: {audio?: boolean, ai?: boolean, physics?: boolean}) {
+        try {
+            this.renderEngine = new WMRenderEngine();
+            if (config?.physics) this.physicsEngine = new WMPhysicsEngine();
+            if (config?.audio) this.audioEngine = new WMAudioEngine();
+            if (config?.ai) this.aiEngine = new WMAIEngine();
+        } catch (e) {
+            console.error(`[WMC error]: Cannot init game engine: ${e.message}`);
+            console.error(e.stack);
+            return;
+        }
+    }
+
+    /** Get access to render engine */
+    public get render(): WMRenderEngine {
+        return this.renderEngine ?? null;
+    }
+
+    /** Get access to physics engine */
+    public get physics(): WMPhysicsEngine {
+        return this.physicsEngine ?? null;
+    }
+
+    /** Get access to audio engine */
+    public get audio(): WMAudioEngine {
+        return this.audioEngine ?? null;
+    }
+
+    /** Get access to artificial intelligence engine */
+    public get ai(): WMAIEngine {
+        return this.aiEngine ?? null;
+    }
+
+    /** Reload render engine */
+    public async reload(): Promise<void> {
+        await this.renderEngine.reloadEngine();
     }
 }
